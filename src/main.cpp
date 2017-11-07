aea7f5a7d1b226c0f768c3b541c7e2e00ebbfbf6
Davide Casella <casella.dvd@gmail.com>
2017-11-07 21:13:19 +0100

    Update main function to take input from stdin

--- src/main.cpp
+++ src/main.cpp
@@ -2,16 +2,22 @@
 
 
 int main(int argc, const char* argv[]) {
-  RBGraph g;
+  if (argc < 2)
+    return 0;
   
-  // TODO: fill g
-  
-  std::list<SignedCharacter> output = reduce(g);
-  
-  std::cout << "C-Reduction: < ";
-  for (auto i = output.begin(); i != output.end(); ++i)
-    std::cout << *i << " ";
-  std::cout << ">" << std::endl;
+  for (int i = 1; i < argc; ++i) {
+    RBGraph g;
+    read_graph(argv[i], g);
+    std::list<SignedCharacter> output = reduce(g);
+    
+    std::cout << "C-Reduction: < ";
+    
+    for (auto i = output.begin(); i != output.end(); ++i) {
+      std::cout << *i << " ";
+    }
+    
+    std::cout << ">" << std::endl;
+  }
   
   return 0;
 }
